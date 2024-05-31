54a199b1512214351fe7792e86d904ce8523ca25
Kieattisak Suparit <kieattisak.sup@devdeva.tech>
2024-05-31 22:54:11 +0700

    Revert "fix: add new className"
    This reverts commit 13f951aca11959b5ebcd52751a7a43b7848566a4.

--- src/components/inputs/InputDropdownSearch2.tsx
+++ src/components/inputs/InputDropdownSearch2.tsx
@@ -30,7 +30,6 @@ interface Props {
   isLoading?: boolean;
   isClear?: boolean;
   readOnly?: boolean;
-  className?: string;
 }
 
 export default function InputDropdownSearch2({
@@ -48,7 +47,6 @@ export default function InputDropdownSearch2({
   isLoading = false,
   isClear = false,
   readOnly = false,
-  className,
 }: Props) {
   const [searchQuery, setSearchQuery] = useState("");
   const [isOpen, setIsOpen] = useState(false);
@@ -139,15 +137,9 @@ export default function InputDropdownSearch2({
         </label>
       )}
       <div
-        className={cn(
-          "relative w-full cursor-pointer rounded-lg border border-gy-stoke text-gy-text",
-          {
-            "mt-2": label,
-            "!border-red-primary": error,
-          },
-          `${height || "global-input-height"}`,
-          className
-        )}
+        className={`relative w-full cursor-pointer rounded-lg border border-gy-stoke text-gy-text ${
+          label && " mt-2 "
+        } ${height || "global-input-height"} ${error && "!border-red-primary"}`}
         ref={drpodownRef}
       >
         <div className="flex  justify-between relative items-center">
